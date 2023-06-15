1c0364f8f87d09a558e91190e22cdc2c5f09501e
verdiyevaselale98@gmail.com <verdiyevaselale98@gmail.com>
2023-06-15 15:59:21 +0400

    js-de problem var

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-const latestRightSide=document.getElementsByClassName("lates-right-side")[0]
+const latestRightSide = document.querySelector(".latest-right-side")
 
 
 window.addEventListener('DOMContentLoaded',melumatlariGetir)
 
 async function melumatlariGetir(){
-    const unvan="http://jsonplaceholder.typicode.com/posts"
+    const unvan = "melumatlar.json"
     try{
         const servereAtilanIstek = await fetch(unvan)
-        const gelenCavabJson = await servereAtilanIstek.json()
+        const gelenCavabJSON = await servereAtilanIstek.json()
         console.log(servereAtilanIstek);
-       for(Let i=0; i<100; i++){
-        latestRightSide.innerHtml '<div class="blog">
-        <span class="metadata">April 16, 2021, 6 mins</span>
-        <h3>${gelenCavabJson[i].title}</h3>
-    <a href="">${gelenCavabJson[1].id}</a>
-    </div>'
+       for(let i=0; i<gelenCavabJSON.lenght; i++){
+        latestRightSide.innerHTML += `<div class="blog">
+        <span class="metadata">${gelenCavabJSON[i].xeberin_paylasildigi_tarix}</span>
+        <h3>${gelenCavabJSON[i].xeberin_basligi}</h3>
+    <a href="">Read the article</a>
+    </div>`
       
    }
     }
+   catch (error) {
+    console.log(error);
+   }
 }
+
+
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+  });
